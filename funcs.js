5d158eb2fd1749235d94c2f1eb084e545180e764
Кирилл <kirill@MacBook-Pro-Kirill.local>
2013-06-07 02:34:15 +0400

    Добавил funcs.js

---
+++ funcs.js
@@ -0,0 +1,11 @@
+/**
+ * Функция возвращает функцию аргумент с поддержкой кеширования!
+ */
+function memoize(f){
+	var cache = {};
+	return function(){
+		var key = arguments.length + Array.prototype.join.call(arguments,',');
+		if(key in cache) return cache[key];
+		else return cache[key] = f.apply(this, arguments);
+	};
+}
